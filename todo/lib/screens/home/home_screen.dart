70c4fa074cfeae9d8617620fb127d5f3da79e465
bonjin-all <bonjin.app@gmail.com>
2021-05-21 17:51:17 +0900

    Init Screen: Home Screen 추가.

---
+++ todo/lib/screens/home/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            CupertinoSliverNavigationBar(
+              largeTitle: Text("할일"),
+            ),
+          ];
+        },
+        body: Container(
+
+        ),
+      ),
+    );
+  }
+}
