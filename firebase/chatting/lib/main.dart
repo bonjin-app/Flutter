332fdd0600b16d307a92a3a755301e0dc0eb1219
bonjin <bonjin.app@gmail.com>
2022-05-23 17:45:48 +0900

    Add Firebase Config

--- firebase/chatting/lib/main.dart
+++ firebase/chatting/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:chatting/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
