c2af825d92a6d7ba558f7079bd8c3d07f9125a50
bonjin <bonjin.app@gmail.com>
2022-06-02 14:34:43 +0900

    Update RowAndColumnScreen: MainAxisAlignment Example

--- tutorials/lib/main.dart
+++ tutorials/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tutorials/screens/custom_scrollview_screen.dart';
+import 'package:tutorials/screens/row_and_column_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CustomScrollViewScreen(),
+      home: RowAndColumnScreen(),
     );
   }
 }
--- tutorials/lib/screens/row_and_column_screen.dart
+++ tutorials/lib/screens/row_and_column_screen.dart
@@ -5,6 +5,70 @@ class RowAndColumnScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
