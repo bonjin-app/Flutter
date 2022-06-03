82c103f61b423fa4e547d89575de24e2f6486f31
bonjin <bonjin.app@gmail.com>
2022-06-03 17:26:14 +0900

    Add Assets

--- speed_code/instagram_redesign/lib/screen/feed_screen.dart
+++ speed_code/instagram_redesign/lib/screen/feed_screen.dart
@@ -10,6 +10,50 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFEDF0F6),
+      body: ListView(
+        physics: AlwaysScrollableScrollPhysics(),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Instagram',
+                  style: TextStyle(fontSize: 32),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => {},
+                      icon: Icon(Icons.live_tv),
+                      iconSize: 30,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      width: 35,
+                      child: IconButton(
+                        onPressed: () => {},
+                        icon: Icon(Icons.send),
+                        iconSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
   }
 }
