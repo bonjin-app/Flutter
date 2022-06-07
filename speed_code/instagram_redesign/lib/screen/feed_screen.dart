48db668c0c02da0fedfcf9ddc14db5ede538940e
bonjin <bonjin.app@gmail.com>
2022-06-07 10:25:26 +0900

    Add ViewPostScreen

--- speed_code/instagram_redesign/lib/screen/feed_screen.dart
+++ speed_code/instagram_redesign/lib/screen/feed_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_redesign/models/post_model.dart';
+
+import 'view_post_screen.dart';
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({Key? key}) : super(key: key);
@@ -8,6 +11,155 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
+  Widget _buildPost(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Container(
+        width: double.infinity,
+        height: 560,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        child: ClipOval(
+                          child: Image(
+                            height: 50,
+                            width: 50,
+                            image: AssetImage(posts[index].authorImageUrl),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      posts[index].authorName,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(posts[index].timeAgo),
+                    trailing: IconButton(
+                      icon: Icon(Icons.more_horiz),
+                      color: Colors.black,
+                      onPressed: () => {},
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ViewPostScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      width: double.infinity,
+                      height: 400,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            offset: Offset(0, 5),
+                            blurRadius: 8,
+                          ),
+                        ],
+                        image: DecorationImage(
+                          image: AssetImage(posts[index].imageUrl),
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.favorite_border),
+                                  iconSize: 30,
+                                ),
+                                Text(
+                                  '2,515',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => ViewPostScreen(),
+                                      ),
+                                    );
+                                  },
+                                  icon: Icon(Icons.chat),
+                                  iconSize: 30,
+                                ),
+                                Text(
+                                  '350',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.bookmark_border),
+                          iconSize: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +174,7 @@ class _FeedScreenState extends State<FeedScreen> {
               children: [
                 Text(
                   'Instagram',
-                  style: TextStyle(fontFamily: "Billabong",fontSize: 32),
+                  style: TextStyle(fontFamily: "Billabong", fontSize: 32),
                 ),
                 Row(
                   children: [
@@ -50,8 +202,46 @@ class _FeedScreenState extends State<FeedScreen> {
           Container(
             width: double.infinity,
             height: 100,
-            color: Colors.red,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: stories.length,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return SizedBox(
+                    width: 10,
+                  );
+                } else {
+                  return Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    margin: EdgeInsets.all(10),
+                    child: CircleAvatar(
+                      child: ClipOval(
+                        child: Image(
+                          height: 60,
+                          width: 60,
+                          image: AssetImage(stories[index]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
           ),
+          _buildPost(0),
+          _buildPost(1),
         ],
       ),
     );
---
+++ speed_code/instagram_redesign/lib/screen/view_post_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewPostScreen extends StatefulWidget {
+  const ViewPostScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewPostScreen> createState() => _ViewPostScreenState();
+}
+
+class _ViewPostScreenState extends State<ViewPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
