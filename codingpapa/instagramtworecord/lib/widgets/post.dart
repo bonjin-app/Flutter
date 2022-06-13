19c6b56ef50a3422cdacaf676011c9b346c78a3e
bonjin <bonjin.app@gmail.com>
2022-06-13 11:01:23 +0900

    Update Post.dart: CircularprogressIndicator 추가

--- codingpapa/instagramtworecord/lib/widgets/post.dart
+++ codingpapa/instagramtworecord/lib/widgets/post.dart
@@ -2,17 +2,33 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatelessWidget {
-  const Post({
+  Post({
     Key? key,
     required this.index,
   }) : super(key: key);
 
   final int index;
+  Size? size;
 
   @override
   Widget build(BuildContext context) {
+    size ??= MediaQuery.of(context).size;
+
     return CachedNetworkImage(
-      imageUrl: 'https://picsum.photos/id/$index/200/300',
+      imageUrl: 'https://picsum.photos/id/$index/2000/2000',
+      placeholder: (context, url) {
+        return Container(
+          child: Center(
+            child: SizedBox(
+              child: CircularProgressIndicator(backgroundColor: Colors.black),
+              width: 60,
+              height: 60,
+            ),
+          ),
+          width: size!.width,
+          height: size!.width,
+        );
+      },
       imageBuilder: (context, imageProvider) {
         return AspectRatio(
           aspectRatio: 1,
