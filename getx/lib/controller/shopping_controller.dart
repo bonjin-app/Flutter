59cee2bdeaf59780861e4d25fa025d09a9a0d92e
bonjin <bonjin.app@gmail.com>
2022-07-08 13:33:11 +0900

    Add ShoppingController dart

---
+++ getx/lib/controller/shopping_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:getx/model/product.dart';
+
+class ShoppingController extends GetxController {
+  var products = <Product>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    fetchData();
+  }
+
+  void fetchData() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    var productData = [
+      Product(id: 1, productName: 'Mouse', productDescription: 'some description about product', price: 40),
+      Product(id: 2, productName: 'Keyboard', productDescription: 'some description about product', price: 200),
+      Product(id: 3, productName: 'Monitor', productDescription: 'some description about product', price: 50),
+      Product(id: 4, productName: 'Ram', productDescription: 'some description about product', price: 37),
+      Product(id: 5, productName: 'Speaker', productDescription: 'some description about product', price: 125),
+    ];
+
+    products.assignAll(productData);
+  }
+}
