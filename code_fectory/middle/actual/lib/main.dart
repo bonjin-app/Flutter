05901909b82aa384a65e4ec66f0537b4ee1d28c0
bonjin <bonjin.app@gmail.com>
2022-09-13 11:04:34 +0900

    Add Assets

--- code_fectory/middle/actual/lib/main.dart
+++ code_fectory/middle/actual/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:actual/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,13 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'NotoSans',
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: LoginScreen(),
+        ));
   }
 }
