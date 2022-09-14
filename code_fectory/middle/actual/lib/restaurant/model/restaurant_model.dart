3cd2bda82589d95096ab27635c3283335f68785f
bonjin <bonjin.app@gmail.com>
2022-09-14 18:02:00 +0900

    json serializable 적용

--- code_fectory/middle/actual/lib/restaurant/model/restaurant_model.dart
+++ code_fectory/middle/actual/lib/restaurant/model/restaurant_model.dart
@@ -1,4 +1,4 @@
-import 'package:actual/common/const/data.dart';
+import 'package:actual/common/utils/data_utils.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'restaurant_model.g.dart';
@@ -14,7 +14,7 @@ class RestaurantModel {
   final String id;
   final String name;
   @JsonKey(
-    fromJson: pathToUrl,
+    fromJson: DataUtils.pathToUrl,
   )
   final String thumbUrl;
   final List<String> tags;
@@ -39,10 +39,6 @@ class RestaurantModel {
   factory RestaurantModel.fromJson(Map<String, dynamic> json) => _$RestaurantModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$RestaurantModelToJson(this);
-
-  static pathToUrl(String value) {
-      return 'http://$ip$value';
-  }
 
   // factory RestaurantModel.fromJson(Map<String, dynamic> json) {
   //   return RestaurantModel(
