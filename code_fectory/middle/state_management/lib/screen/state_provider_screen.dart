ae7efa2abde766115687390337752904dc0d8541
bonjin <bonjin.app@gmail.com>
2022-09-16 08:56:15 +0900

    Update StateProvider

--- code_fectory/middle/state_management/lib/screen/state_provider_screen.dart
+++ code_fectory/middle/state_management/lib/screen/state_provider_screen.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_management/riverpod/state_provider.dart';
 
 import '../layout/default_layout.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
       title: 'StateProviderScreen',
-      body: Column(
-        children: [],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(onPressed: () {
+              ref.read(numberProvider.notifier).update((state) => ++state);
+            }, child: Text('UP')),
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => _NextScreen()));
+            }, child: Text('NEXT'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(onPressed: () {
+              ref.read(numberProvider.notifier).update((state) => ++state);
+            }, child: Text('UP'))
+          ],
+        ),
       ),
     );
   }
