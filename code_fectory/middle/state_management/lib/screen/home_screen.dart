6d17ea23aa440d7134102c2aad5183a256936648
bonjin <bonjin.app@gmail.com>
2022-09-16 13:12:20 +0900

    Update ListenProvider

--- code_fectory/middle/state_management/lib/screen/home_screen.dart
+++ code_fectory/middle/state_management/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:state_management/screen/auto_dispose_modifier_screen.dart';
 import 'package:state_management/screen/family_modifier_screen.dart';
 import 'package:state_management/screen/future_provider_screen.dart';
+import 'package:state_management/screen/listen_provider_screen.dart';
 import 'package:state_management/screen/state_provider_screen.dart';
 import 'package:state_management/screen/stream_provider_screen.dart';
 
@@ -52,6 +53,12 @@ class HomeScreen extends StatelessWidget {
               Navigator.of(context).push(MaterialPageRoute(builder: (_) => AutoDisposeModifierScreen()));
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => ListenProviderScreen()));
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),
