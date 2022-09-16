b439d0a0f22509ade754c85d3287f69d20485862
bonjin <bonjin.app@gmail.com>
2022-09-16 15:37:55 +0900

    Update repository Provider

--- code_fectory/middle/actual/lib/restaurant/view/restaurant_screen.dart
+++ code_fectory/middle/actual/lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/common/dio/dio.dart';
+import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/repository/restaurant_repository.dart';
@@ -10,27 +11,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class RestaurantScreen extends ConsumerWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
-  Future<List<RestaurantModel>> paginateRestaurant(WidgetRef ref) async {
-    final dio = ref.watch(dioProvider);
-    final response = await RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant').paginate();
-    return response.data;
-  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: FutureBuilder<List<RestaurantModel>>(
-          future: paginateRestaurant(ref),
-          builder: (context, AsyncSnapshot<List<RestaurantModel>> snapshot) {
+        child: FutureBuilder<CursorPagination<RestaurantModel>>(
+          future: ref.watch(restaurantRepositoryProvider).paginate(),
+          builder: (context, AsyncSnapshot<CursorPagination<RestaurantModel>> snapshot) {
             if (!snapshot.hasData) {
               return const Center(child: CircularProgressIndicator());
             }
 
             return ListView.separated(
+              itemCount: snapshot.data!.data.length,
               itemBuilder: (context, index) {
-                final item = snapshot.data![index];
+                final item = snapshot.data!.data[index];
 
                 return GestureDetector(
                   onTap: () {
@@ -46,7 +43,6 @@ class RestaurantScreen extends ConsumerWidget {
               separatorBuilder: (context, index) {
                 return const Divider();
               },
-              itemCount: snapshot.data!.length,
             );
           },
         ),
