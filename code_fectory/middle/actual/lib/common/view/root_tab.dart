0e79d69e4681dc3a69cfeeb90c566fdb43a187eb
bonjin <bonjin.app@gmail.com>
2022-09-19 14:22:12 +0900

    product repository, provider

--- code_fectory/middle/actual/lib/common/view/root_tab.dart
+++ code_fectory/middle/actual/lib/common/view/root_tab.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/product/view/product_tab.dart';
 import 'package:actual/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -44,7 +45,7 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
         controller: controller,
         children: [
           RestaurantScreen(),
-          Container(child: Text('음식'),),
+          ProductTab(),
           Container(child: Text('주문'),),
           Container(child: Text('프로필'),),
         ],
