9385ad69a97c565a820604726e82ea4928787b13
bonjin <bonjin.app@gmail.com>
2022-09-19 11:04:29 +0900

    add rating model, repository

---
+++ code_fectory/middle/actual/lib/rating/component/rating_card.dart
@@ -0,0 +1,149 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+class RatingCard extends StatelessWidget {
+  // NetworkImage
+  // AssetImage
+  // CircleAvatar
+  final ImageProvider avatarImage;
+  final List<Image> images;
+  final int rating;
+  final String email;
+  final String content;
+
+  const RatingCard({
+    Key? key,
+    required this.avatarImage,
+    required this.images,
+    required this.rating,
+    required this.email,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _Header(
+          avatarImage: avatarImage,
+          rating: rating,
+          email: email,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        _Body(
+          content: content,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        if (images.isNotEmpty)
+          SizedBox(
+            height: 100,
+            child: _Images(images: images),
+          ),
+      ],
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  final ImageProvider avatarImage;
+  final int rating;
+  final String email;
+
+  const _Header({
+    Key? key,
+    required this.avatarImage,
+    required this.rating,
+    required this.email,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: avatarImage,
+          radius: 12,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              email,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        ...List.generate(
+          5,
+          (index) => Icon(
+            index < rating ? Icons.star : Icons.star_border_outlined,
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final String content;
+
+  const _Body({
+    Key? key,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: Text(
+            content,
+            style: const TextStyle(
+              color: BODY_TEXT_COLOR,
+              fontSize: 14,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Images extends StatelessWidget {
+  final List<Image> images;
+
+  const _Images({
+    Key? key,
+    required this.images,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: images
+          .mapIndexed(
+            (i, e) => Padding(
+              padding: EdgeInsets.only(right: i == images.length - 1 ? 0 : 16),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: e,
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
