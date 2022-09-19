d98e4e4a7a0305d72076d793f4e480797017c73d
bonjin <bonjin.app@gmail.com>
2022-09-19 14:48:22 +0900

    pagination list view 생성

--- code_fectory/middle/actual/lib/common/view/root_tab.dart
+++ code_fectory/middle/actual/lib/common/view/root_tab.dart
@@ -1,6 +1,6 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
-import 'package:actual/product/view/product_tab.dart';
+import 'package:actual/product/view/product_screen.dart';
 import 'package:actual/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -45,7 +45,7 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
         controller: controller,
         children: [
           RestaurantScreen(),
-          ProductTab(),
+          ProductScreen(),
           Container(child: Text('주문'),),
           Container(child: Text('프로필'),),
         ],
--- code_fectory/middle/actual/lib/restaurant/view/restaurant_screen.dart
+++ code_fectory/middle/actual/lib/restaurant/view/restaurant_screen.dart
@@ -1,99 +1,34 @@
-import 'package:actual/common/model/cursor_pagination_model.dart';
-import 'package:actual/common/utils/pagination_utils.dart';
+import 'package:actual/common/component/pagination_list_view.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerStatefulWidget {
-  const RestaurantScreen({Key? key}) : super(key: key);
-
-  @override
-  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
-}
-
-class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
-  final ScrollController controller = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller.addListener(scrollListener);
-  }
-
-  void scrollListener() {
-    PaginationUtils.paginate(
-      controller: controller,
-      provider: ref.read(
-        restaurantProvider.notifier,
-      ),
-    );
-  }
+class RestaurantScreen extends StatelessWidget {
+  const RestaurantScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final data = ref.watch(restaurantProvider);
-
-    if (data is CursorPaginationLoading) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    if (data is CursorPaginationError) {
-      return Center(
-        child: Text(data.message),
-      );
-    }
-
-    // CursorPagination
-    // CursorPaginationFetchingMore
-    // CursorPaginationReFetching
-    final cp = data as CursorPagination;
-
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView.separated(
-          controller: controller,
-          itemCount: cp.data.length + 1,
-          itemBuilder: (context, index) {
-            if (index == cp.data.length) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Center(
-                  child: data is CursorPaginationFetchingMore
-                      ? CircularProgressIndicator()
-                      : Text('마지막 데이터 입니다.'),
+    return PaginationListView(
+      provider: restaurantProvider,
+      itemBuilder: <RestaurantModel>(_, index, model) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => RestaurantDetailScreen(
+                  id: model.id,
                 ),
-              );
-            }
-
-            final item = cp.data[index];
-
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => RestaurantDetailScreen(
-                      id: item.id,
-                    ),
-                  ),
-                );
-              },
-              child: RestaurantCard.fromModel(
-                model: item,
               ),
             );
           },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
-        ),
-      ),
+          child: RestaurantCard.fromModel(
+            model: model,
+          ),
+        );
+      },
     );
   }
 }
